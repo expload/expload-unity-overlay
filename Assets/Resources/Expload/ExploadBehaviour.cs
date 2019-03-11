39a3674e528549e6efd5900521d5d36f55ae6d67
Vladimir Berezkin <vb@mytc.io>
2019-03-11 19:58:39 +0300

    Check that UI already exists

--- Assets/Resources/Expload/ExploadBehaviour.cs
+++ Assets/Resources/Expload/ExploadBehaviour.cs
@@ -9,29 +9,27 @@ public class ExploadBehaviour : MonoBehaviour
     void Start()
     {
 #if !UNITY_EDITOR
-        GameObject canvas = new GameObject("ExploadCanvas");
-        canvas.AddComponent<Canvas>();
-        canvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
-        canvas.GetComponent<Canvas>().sortingOrder = 99;
-        DontDestroyOnLoad(canvas);
-        GameObject raw_image = new GameObject("OverlayImage");
-        raw_image.transform.parent = canvas.transform;
-        raw_image.AddComponent<RawImage>();
+        if (!GameObject.Find("ExploadCanvas"))
+        {
+            GameObject canvas = new GameObject("ExploadCanvas");
+            canvas.AddComponent<Canvas>();
+            canvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.GetComponent<Canvas>().sortingOrder = 99;
+            DontDestroyOnLoad(canvas);
+            GameObject raw_image = new GameObject("OverlayImage");
+            raw_image.transform.parent = canvas.transform;
+            raw_image.AddComponent<RawImage>();
         
-        raw_image.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
-        raw_image.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
-        raw_image.GetComponent<RectTransform>().anchorMax = new Vector2(1, 1);
-        raw_image.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
-        raw_image.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
-        raw_image.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
-        raw_image.GetComponent<RawImage>().color = new Color(1, 1, 1, 0.5f);
-        raw_image.AddComponent<Expload.OffscreenCEF>();
+            raw_image.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
+            raw_image.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+            raw_image.GetComponent<RectTransform>().anchorMax = new Vector2(1, 1);
+            raw_image.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+            raw_image.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
+            raw_image.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
+            raw_image.GetComponent<RawImage>().color = new Color(1, 1, 1, 0.5f);
+            raw_image.AddComponent<Expload.OffscreenCEF>();
+        }
 #endif
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
+
